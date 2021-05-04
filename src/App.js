b0c126a4260252ef90f01f0a125433f336dd0e9e
amirmohammad <mamad198585@gmail.com>
2021-05-04 18:02:31 +0430

    two varibales created in global

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
       <div className="global">
         {
           Object.entries(global_data).map(item => {
-            
+            const [title,number] = item;
           })
         }
       </div>
