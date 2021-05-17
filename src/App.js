06b275f534b11fbc3ca717c1b4f6a202dd72a58a
amirmohammad <mamad198585@gmail.com>
2021-05-17 19:07:04 +0430

    SideBar.js and its initial code added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import SideBar from './components/SideBar'
 
 const App = () => {
   return (
     <>
-      
+      <SideBar/>
     </>
   )
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            <h1>Countries</h1>
+        </div>
+    )
+}
+
+export default SideBar
